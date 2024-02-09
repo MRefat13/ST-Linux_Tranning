d016a92e2f3684f83a41d28360439f9273918c57
MohamedRefat13 <mo7amedrefaat2001@gmail.com>
2024-02-09 19:53:24 +0200

    Adding new version of picoShell.c
    This improved version can redirect the output using special character
    [arrow >]
    example ls > file1
    
    Also,this version can deal with escape character "
    example touch " file 1"

--- 04_PicoShellv2/picoShell.c
+++ 04_PicoShellv2/picoShell.c
@@ -7,170 +7,262 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define PATH_MAX (200)
-void parsing(char *pInput, char *arr_tokens[]);
+/* Delimiter for tokenizing input on spaces */
+#define SPACE_DELIMETER (" ") 
+
+/* Delimiter for tokenizing input within double quotes */
+#define DQOTES_DELIMETER ("\"")
+
+/*  Maximum length of a file path */
+#define PATH_MAX (200)     
+
+/* Position of the output redirection symbol (>) */
+int arrow_poss = 0;
+
+/**
+ * @brief Function is used to parse the input into tokens
+ * 
+ * @param pInput  string of the input line by the user 
+ * @param myargv  array of tokens to store the tokens in it
+ * @return int    number of tokens argc 
+ * @note  the return from the function (argc) my be diff from the return from getTokenNumber
+ *          because the parsing function doesn't count special characters [ " and > ] 
+ */
+int parsing(char *pInput, char *myargv[]);
+
+/**
+ * @brief Function to count the number of tokens in the input
+ * 
+ * @param cmd string of the input line by the user
+ * @return int number of tokens(arguments) argc
+ */
 int getTokenNumber(char *cmd);
 
 int main()
 {
-    int iterator = 0, tokensNum = 0;
-    char *cmd = NULL;
-    size_t n = 0;
-    while (1) {
-	iterator = 0;
+  int iterator = 0, myargc = 0;
+  char *cmd = NULL;
+  size_t n = 0;
+  while (1)
+  {
+    iterator = 0;
 
-	/* Read the Input from the user */
-	printf("O2mor ya gameel >> ");
-	int size = getline(&cmd, &n, stdin);
+    /* Read the Input from the user */
+    printf("O2mor ya gameel >> ");
+    int size = getline(&cmd, &n, stdin);
 
-	/* inset a '\0' at the eand of the input */
-	cmd[size - 1] = 0;
+    /* inset a '\0' at the and of the input */
+    cmd[size - 1] = 0;
 
-	/* get Number of tokens[argument] --> argc */
-	int tokensNum = getTokenNumber(cmd);	// number of takens
-	if (tokensNum == 0) {
-	    continue;
-	}
-
-	/* increment the tokensNum to add a null at the end of the tokens
-	 *  for the exec function
-	 * */
-	tokensNum++;
-
-	/* Allocate a memory for the array of the argument */
-	char **arr_tokens;
-	arr_tokens = (char **) malloc( (tokensNum) * sizeof(char *));
-
-	/*parse the input line */
-	parsing(cmd, arr_tokens);
-	
-	/*Check for the O/P redirection >*/
-	
-	/*
-	 * TO DO:
-	 *
-	for(iterator=0 ; iterator< tokensNum -1  ; iterator++)
-	{
-
-		if (strcmp(arr_tokens[iterator], ">") == 0) {
-			int fd = open (arr_tokens[iterator+1], O_WRONLY | O_CREAT| O_TRUNC, 0644);
-			if (fd > 0 )
-			{
-
-				int fd2 = dup2(fd,1);
-				if (fd2 < 0 )
-				{
-				 	printf("Failed to redirect output.\n");
-				}
-				close(fd);
-				break;
-				
-			}else
-			{
-				printf("Can not open of creat the redirection file\n");
-		
-			}
-
-		}
-	}
-	*/
-	if (strcmp(arr_tokens[0], "pwd\0") == 0) {
-	    char path[PATH_MAX];
-	    size_t size = PATH_MAX;
-	    if (getcwd(path, size) != NULL) {
-		printf(">>%s\n", path);
-	    }
-
-	} else if (strcmp(arr_tokens[0], "echo") == 0) {
-	    iterator = 1;
-	    while (arr_tokens[iterator] != NULL) {
-		printf("%s ", arr_tokens[iterator]);
-		iterator++;
-	    }
-	    printf("\n");
-	} else if (strcmp(arr_tokens[0], "cd") == 0) {
-
-	    if (chdir(arr_tokens[1]) != 0) {
-		printf(">>invalid path\n");
-	    }
-	} else if (strcmp(arr_tokens[0], "exit") == 0) {
-	    printf(">>Goodbye\n");
-	    break;
-	} else {
-	    pid_t returned_pid = fork();
-	    if (returned_pid > 0) {
-		// PARENT
-		int wstatus;
-		//printf("PARENT: %d\n",getpid());
-		wait(&wstatus);
-	    } else if (returned_pid == 0) {
-		// CHILD
-
-		arr_tokens[tokensNum ] = NULL;
-		execvp(arr_tokens[0], arr_tokens);
-		printf("Please Enter a valid Command\n");
-		return 0;
-	    }
-	}
-
-
-	/* free the array of the input arguments --> argv */
-        for(iterator=0 ; iterator< tokensNum  ; iterator++)
-        {	
-
-                free(arr_tokens[iterator]);
-		arr_tokens[iterator]=NULL;
-        }
-	/* free the array os the argument it self */
-	free(arr_tokens);
-	arr_tokens = NULL;
-
-	/* Free the CMD */
-	free(cmd);
-	cmd = NULL;
-	n = 0;
-    }
-
-}
-
-void parsing(char *pInput, char *arr_tokens[])
-{
-    int iterator = 0;
-    char* token = strtok(pInput, " ");
-    while( token != NULL )
+    /* get Number of tokens[argument] --> argc */
+    int myargc = getTokenNumber(cmd); // number of tokens
+    if (myargc == 0)
     {
-    	arr_tokens[iterator] = strdup(token);
-
-    	iterator++;
-    	token = strtok(NULL, " ");
+      continue;
     }
-	
+
+    /* increment  myargc to add a null at the end of the tokens
+     *  for the exec function
+     * */
+    myargc++;
+
+    /* Allocate a memory for the array of the argument */
+    char **myargv;
+    myargv = (char **)malloc((myargc) * sizeof(char *));
+
+    /*parse the input line */
+    int myargc_new = parsing(cmd, myargv);
+
+    /*Check for the O/P redirection > */
+    int fd;
+    char RedirFile_Flag = 0;
+    int Output_Fd = 1; // STDOUT
+
+    /*Handel O/P Redirection */
+    if (arrow_poss != 0)
+    {
+
+      fd = open(myargv[arrow_poss],
+                O_WRONLY | O_CREAT | O_TRUNC, 0644);
+      if (fd > 0)
+      {
+        RedirFile_Flag = 1;
+      }
+      else
+      {
+        printf("Can not open of creat the redirection file\n");
+      }
+      arrow_poss = 0;
+    }
+    if (RedirFile_Flag == 1)
+    {
+      Output_Fd = fd;
+    }
+
+    if (strcmp(myargv[0], "pwd\0") == 0)
+    {
+      char path[PATH_MAX];
+      size_t size = PATH_MAX;
+      if (getcwd(path, size) != NULL)
+      {
+        dprintf(Output_Fd, ">>%s\n", path);
+      }
+    }
+    else if (strcmp(myargv[0], "echo") == 0)
+    {
+      iterator = 1;
+      for (iterator = 1; iterator < myargc_new - 1; iterator++)
+      {
+        dprintf(Output_Fd, "%s ", myargv[iterator]);
+      }
+      dprintf(Output_Fd, "\n");
+    }
+    else if (strcmp(myargv[0], "cd") == 0)
+    {
+
+      if (chdir(myargv[1]) != 0)
+      {
+        dprintf(1, ">>invalid path\n");
+      }
+    }
+    else if (strcmp(myargv[0], "exit") == 0)
+    {
+      dprintf(1, ">>Goodbye\n");
+      break;
+    }
+    else
+    {
+      pid_t returned_pid = fork();
+      if (returned_pid > 0)
+      {
+        // PARENT
+        int wstatus;
+        wait(&wstatus);
+      }
+      else if (returned_pid == 0)
+      {
+        // CHILD
+        myargv[myargc_new] = NULL;
+        if (RedirFile_Flag == 1)
+        {
+          int fd2 = dup2(fd, 1);
+          if (fd2 < 0)
+          {
+            dprintf(1, "Failed to redirect the output\n");
+          }
+          else
+          {
+
+            char *new_envp[] = {NULL};
+            execvp(myargv[0], new_envp);
+            dprintf(1, "Please Enter a valid Command\n");
+          }
+        }
+        else
+        {
+
+          execvp(myargv[0], myargv);
+          dprintf(1, "Please Enter a valid Command\n");
+        }
+
+        return 0;
+      }
+    }
+
+    if (RedirFile_Flag == 1)
+    {
+      close(fd);
+    }
+
+    /* free the array of the input arguments --> argv */
+    for (iterator = 0; iterator < myargc; iterator++)
+    {
+
+      free(myargv[iterator]);
+      myargv[iterator] = NULL;
+    }
+
+    /* free the array of  the argument it self */
+    free(myargv);
+    myargv = NULL;
+
+    /* Free the CMD */
+    free(cmd);
+    cmd = NULL;
+    n = 0;
+  }
 }
 
+int parsing(char *pInput, char *myargv[])
+{
+  int iterator = 0;
+  char *pTemp = 0;
+  char delimeter[] = SPACE_DELIMETER;
+  char openFlag = 0;
+  char *token = strtok(pInput, " ");
+  while (token != NULL)
+  {
+    /* if the token is a [ " or > ] do not store it in myargv */
+    if ((strcmp(token, "\"") != 0) &&
+        (strcmp(token, ">") != 0))
+    {
+      myargv[iterator] = strdup(token);
+      iterator++;
+    }
+    if ((strcmp(token, ">") == 0))
+    {
+      arrow_poss = iterator;
+    }
+
+    if (openFlag == 1 || (strcmp(token, "\"") == 0))
+    {
+
+      if (openFlag == 0)
+      {
+        openFlag = 1;
+        strcpy(delimeter, DQOTES_DELIMETER);
+      }
+      else if (openFlag == 1)
+      {
+        openFlag = 0;
+        strcpy(delimeter, SPACE_DELIMETER);
+      }
+    }
+    token = strtok(NULL, delimeter);
+  }
+  // return number of argument
+  return iterator;
+}
 
 int getTokenNumber(char *cmd)
 {
-    int iterator1 = 0;
-    int cnt = 0;
-    char arg_flag = 0;
-    while (cmd[iterator1] != '\0') {
-	// Skip leading spaces before any token
-	while (cmd[iterator1] == ' ' && cmd[iterator1] != '\0') {
-	    iterator1++;
-	}
-
-	while (cmd[iterator1] != ' ' && cmd[iterator1] != '\0') {
-	    iterator1++;
-	    if (arg_flag == 0) {
-		arg_flag = 1;
-	    }
-	}
-	if (arg_flag == 1) {
-	    arg_flag = 0;
-	    cnt++;
-	}
-	iterator1++;
-
+  int iterator1 = 0;
+  int cnt = 0;
+  char arg_flag = 0;
+  while (cmd[iterator1] != '\0')
+  {
+    // Skip leading spaces before any token
+    while (cmd[iterator1] == ' ' && cmd[iterator1] != '\0')
+    {
+      iterator1++;
     }
-    return cnt;
+
+    // Skip the token and increment cnt by one
+    while (cmd[iterator1] != ' ' && cmd[iterator1] != '\0')
+    {
+      iterator1++;
+      if (arg_flag == 0)
+      {
+        arg_flag = 1;
+      }
+    }
+    if (arg_flag == 1)
+    {
+      arg_flag = 0;
+      cnt++;
+    }
+    iterator1++;
+  }
+  // return number of arguments
+  return cnt;
 }
